670854551dc97033d75401f69b06e682b842dbcb
Martin Brochhaus <mbrochh@gmail.com>
2012-10-12 07:27:49 +0800

    Added UserMediaImageFormMixin

--- user_media/forms.py
+++ user_media/forms.py
@@ -1,7 +1,36 @@
 """Forms for the ``django-user-media`` app."""
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_lazy as _
 
 from user_media.models import UserMediaImage
+
+
+class UserMediaImageFormMixin(object):
+    """TODO: Write docs"""
+
+    image_label = _('Image')
+    require_user_media_image = False
+
+    def __init__(self, *args, **kwargs):
+        super(UserMediaImageFormMixin, self).__init__(*args, **kwargs)
+        self.fields['user_media_image'] = forms.ImageField(
+            required=self.require_user_media_image,
+            label=self.image_label,
+        )
+
+    def save(self, *args, **kwargs):
+        instance = super(UserMediaImageFormMixin, self).save(*args, **kwargs)
+        umedia_image = self.cleaned_data['user_media_image']
+        if umedia_image:
+            image = UserMediaImage()
+            image.user = instance.user
+            image.content_type = ContentType.objects.get_for_model(
+                instance)
+            image.object_id = instance.pk
+            image.image = umedia_image
+            image.save()
+        return instance
 
 
 class UserMediaImageForm(forms.ModelForm):
