ee8747e9d6504594fa402899bcdb43ceb5e712ca
Martin Brochhaus <mbrochh@gmail.com>
2012-12-08 21:04:56 +0800

    Added admin for the UserMediaImage model.

---
+++ user_media/admin.py
@@ -0,0 +1,16 @@
+"""Admin classes for the ``django-user-media`` app."""
+from django.contrib import admin
+
+from user_media.models import UserMediaImage
+
+
+class UserMediaImageAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'content_type', 'object_id', 'image', )
+    list_filter = ('content_type', )
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'User'
+
+
+admin.site.register(UserMediaImage, UserMediaImageAdmin)
